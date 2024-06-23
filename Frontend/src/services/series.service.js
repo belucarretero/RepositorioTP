d2a5056cffe6e4a1421c587b6d9ecfa467c1547f
María Belén Carretero <164357431+belucarretero@users.noreply.github.com>
2024-06-23 01:42:10 -0300

    Create series.service.js

---
+++ Frontend/src/services/series.service.js
@@ -0,0 +1,39 @@
+import httpService from "./http.service";
+//const urlResource = "https://labsys.frc.utn.edu.ar/dds-express/api/peliculas";
+
+// mas adelante podemos usar un archivo de configuracion para el urlResource
+ import {config} from "../config";
+ const urlResource = config.urlResourceSeries;
+
+
+async function Buscar(Nombre, Activo, Pagina) {
+  const resp = await httpService.get(urlResource, {
+    params: { Nombre, Activo, Pagina },
+  });
+  return resp.data;
+}
+
+
+async function BuscarPorId(item) {
+  const resp = await httpService.get(urlResource + "/" + item.CodigoSerie);
+  return resp.data;
+}
+
+
+async function ActivarDesactivar(item) {
+  await httpService.delete(urlResource + "/" + item.CodigoSerie);
+}
+
+
+async function Grabar(item) {
+  if (item.CodigoSerie === 0) {
+    await httpService.post(urlResource, item);
+  } else {
+    await httpService.put(urlResource + "/" + item.CodigoSerie, item);
+  }
+}
+
+
+export const seriesService = {
+  Buscar,BuscarPorId,ActivarDesactivar,Grabar
+};
